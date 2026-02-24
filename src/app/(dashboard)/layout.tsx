3b7770a1a2c4370b23f84287e0aff1416dc138d0
Diksha <deekshacdac666@gmail.com>
2026-02-24 12:01:10 +0530

    07: Navbar-setup

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+import {DashboardNavbar} from "@/modules/dashboard/ui/components/dashboard-navbar";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 
      interface Props{
@@ -9,13 +10,13 @@ import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-si
      const Layout = ({children }: Props) => {
                 return (
                     <SidebarProvider>
-                        <DashboardSidebar/>
-                        <main className="flex flex-col h-screen w-screen bg-muted"> {children}</main>
-                        
-                    </SidebarProvider>
-                       
-                  
-                  );
-              }
-               
-              export default Layout;                    
+                      <DashboardSidebar />
+      <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
+ 
+export default Layout;                    
